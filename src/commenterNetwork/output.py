14ee5da69ce20803b2e625cb424a91f14823142d
Thomas Marcoux <txmarcoux@ualr.edu>
2022-04-02 15:55:58 -0500

    Added docstring

--- src/commenterNetwork/output.py
+++ src/commenterNetwork/output.py
@@ -4,6 +4,7 @@ from ..dataManager import checkPathExists, getFilePath
 
 
 def saveNetwork(settings, file, G):
+    """Save the networkx graph to disk."""
     file_path = getFilePath(settings, file)
     checkPathExists(file_path)
     nx.write_gexf(G, file_path)
