f57e731c0e0b4ad35d4fb1f2dc66de972c02a838
Thomas Marcoux <txmarcoux@ualr.edu>
2022-04-02 21:56:35 -0500

    Reorganized data manager & added DB functions & saving/loading messages

--- src/dataManager.py
+++ src/dataManager.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import pickle
 import pymysql
 import os
+from tqdm import tqdm
 
 PROCESSED_DATA_FOLDER = "processed_data"
 OUTPUT_FOLDER = "Output"
@@ -20,6 +21,7 @@ def save_file(destination, content):
         os.makedirs(os.path.dirname(destination))
     with open(destination, 'wb') as out:
         pickle.dump(content, out)
+        print(f'Saving {destination}.')
 
 
 def load_file(file_path):
@@ -27,6 +29,7 @@ def load_file(file_path):
     if not os.path.isfile(file_path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file_path)
     with open(file_path, 'rb') as f:
+        print(f'Loading {file_path}.')
         return pickle.load(f)
 
 
@@ -58,96 +61,6 @@ def checkDirectory(output_dir):
     """Create directory if it does not exist."""
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
-
-
-### TEMP FILES ###
-
-
-def save_tmp(settings, suffix, content):
-    """Pickle given structure to temporary folder."""
-    destination = getFilePath(settings, suffix)
-    save_file(destination, content)
-
-
-def load_tmp(settings, suffix):
-    """Load pickled file from temporary folder."""
-    source = getFilePath(settings, suffix)
-    return load_file(source)
-
-
-### DATAFRAMES ###
-
-def save_df(settings, file, df):
-    """Saves dataframe to temporary folder."""
-    file_path = getFilePath(settings, file)
-    if not os.path.exists(os.path.dirname(file_path)):
-        os.makedirs(os.path.dirname(file_path))
-    df.to_pickle(file_path)
-
-
-def load_df(settings, file):
-    """Loads dataframe from temporary folder."""
-    file_path = getFilePath(settings, file)
-    if not os.path.isfile(file_path):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file_path)
-    return pd.read_pickle(file_path)
-
-
-### DATABASE ###
-
-
-def get_connection(db_settings):
-    connection = pymysql.connect(
-        host=db_settings['host'],
-        user=db_settings['user'],
-        password=db_settings['password'],
-        db=db_settings['db'],
-        charset=db_settings['charset'] if 'charset' in db_settings else 'utf8',
-        use_unicode=True,
-    )
-    return connection
-
-
-def get_query(db_settings):
-    query = ""
-    filePath = os.path.join(os.getcwd(), db_settings['query'])
-    if os.path.isfile(filePath):
-        with open(filePath, 'r', encoding='utf-8') as f:
-            query = f.read().strip('/r/n')
-    else:
-        query = db_settings['query']
-    return query
-
-
-def read_file(settings, dataFile, use_all_columns=False):
-    data_type = Path(dataFile).suffix
-    encoding = settings['encoding'] if 'encoding' in settings else 'utf-8'
-    if use_all_columns:
-        selected_columns = None
-    else:
-        selected_columns = [settings['corpusFieldName']]
-    if 'dateFieldName' in settings:
-        selected_columns.append(settings['dateFieldName'])
-    if 'idFieldName' in settings:
-        selected_columns.append(settings['idFieldName'])
-    if data_type == '.json':
-        json_orientation = settings['json_orientation'] if 'json_orientation' in settings else None
-        df = pd.read_json(dataFile, orient=json_orientation, encoding=encoding)
-        df = df[selected_columns]
-    elif data_type == '.csv':
-        df = pd.read_csv(dataFile, na_filter=False, usecols=selected_columns)
-    elif data_type == '.xlsx':
-        df = pd.read_excel(dataFile, na_filter=False, usecols=selected_columns)
-    else:
-        raise Exception
-    total_items = df.shape[0]
-    df.dropna(inplace=True)
-    nan_items = total_items - df.shape[0]
-    items_loaded = humanize.intcomma(df.shape[0])
-    total_items = humanize.intcomma(total_items)
-    nan_items = humanize.intcomma(nan_items)
-    print(f"Loading {items_loaded} items from {dataFile} - {total_items} total items, {nan_items} NaN values were detected and removed.")
-    return df
 
 
 def read_data(settings):
@@ -190,6 +103,122 @@ def read_data(settings):
     df = df.set_index([dateFieldName])
     df.sort_index(inplace=True)
     return df
+
+
+### TEMP FILES ###
+
+
+def save_tmp(settings, suffix, content):
+    """Pickle given structure to temporary folder."""
+    destination = getFilePath(settings, suffix)
+    save_file(destination, content)
+
+
+def load_tmp(settings, suffix):
+    """Load pickled file from temporary folder."""
+    source = getFilePath(settings, suffix)
+    return load_file(source)
+
+
+### DATAFRAMES ###
+
+def save_df(settings, file, df):
+    """Saves dataframe to temporary folder."""
+    file_path = getFilePath(settings, file)
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    df.to_pickle(file_path)
+    print(f'Save {file}.')
+
+
+def load_df(settings, file):
+    """Loads dataframe from temporary folder."""
+    file_path = getFilePath(settings, file)
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file_path)
+    print(f'Loading {file}.')
+    return pd.read_pickle(file_path)
+
+
+### DATABASE ###
+
+
+def get_connection(db_settings):
+    connection = pymysql.connect(
+        host=db_settings['host'],
+        user=db_settings['user'],
+        password=db_settings['password'],
+        db=db_settings['db'],
+        charset=db_settings['charset'] if 'charset' in db_settings else 'utf8',
+        use_unicode=True,
+    )
+    return connection
+
+
+def get_query(db_settings):
+    query = ""
+    filePath = os.path.join(os.getcwd(), db_settings['query'])
+    if os.path.isfile(filePath):
+        with open(filePath, 'r', encoding='utf-8') as f:
+            query = f.read().strip('/r/n')
+    else:
+        query = db_settings['query']
+    return query
+
+
+def countQueryItems(db_connector, count_query):
+    """Return total number of items for given query."""
+    cur = db_connector.cursor()
+    cur.execute(count_query)
+    return cur.fetchall()[-1][-1]
+
+
+def fetchData(db_connector, query, table_name, columns, chunksize, total):
+    out_df = None
+    progress = tqdm(total=total, desc=f'Collecting {table_name}...')
+    chunks = pd.read_sql(query, db_connector, columns=columns, chunksize=chunksize)
+    total = 0
+    for df in chunks:
+        out_df = pd.concat([out_df, df])
+        total += df.shape[0]
+        progress.set_description(desc=f"Collecting {table_name} : [Last packet size: {humanize.intcomma(df.shape[0])} / Total : {humanize.intcomma(total)}]", refresh=True)
+        progress.update(chunksize)
+    return out_df
+
+
+### FILES ###
+
+
+def read_file(settings, dataFile, use_all_columns=False):
+    data_type = Path(dataFile).suffix
+    encoding = settings['encoding'] if 'encoding' in settings else 'utf-8'
+    if use_all_columns:
+        selected_columns = None
+    else:
+        selected_columns = [settings['corpusFieldName']]
+    if 'dateFieldName' in settings:
+        selected_columns.append(settings['dateFieldName'])
+    if 'idFieldName' in settings:
+        selected_columns.append(settings['idFieldName'])
+    if data_type == '.json':
+        json_orientation = settings['json_orientation'] if 'json_orientation' in settings else None
+        df = pd.read_json(dataFile, orient=json_orientation, encoding=encoding)
+        df = df[selected_columns]
+    elif data_type == '.csv':
+        df = pd.read_csv(dataFile, na_filter=False, usecols=selected_columns)
+    elif data_type == '.xlsx':
+        df = pd.read_excel(dataFile, na_filter=False, usecols=selected_columns)
+    else:
+        raise Exception
+    total_items = df.shape[0]
+    df.dropna(inplace=True)
+    nan_items = total_items - df.shape[0]
+    items_loaded = humanize.intcomma(df.shape[0])
+    total_items = humanize.intcomma(total_items)
+    nan_items = humanize.intcomma(nan_items)
+    print(f"Loading {items_loaded} items from {dataFile} - {total_items} total items, {nan_items} NaN values were detected and removed.")
+    return df
+
 
 ### YOUTUBE ###
 
