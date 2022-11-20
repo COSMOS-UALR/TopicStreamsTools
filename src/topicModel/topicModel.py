4db4741f3d9c7bb7cea975fdde4b175880cfce96
Thomas Marcoux <txmarcoux@ualr.edu>
2022-11-20 16:00:40 -0600

    Removed obsolete node description

--- src/topicModel/topicModel.py
+++ src/topicModel/topicModel.py
@@ -16,13 +16,11 @@ class TopicModelNode:
 
 
     def run(self, previous_node_output):
-        print(f"BEGIN {self.settings['node']}")
         model = self.getModel()
         corpus_df = model.loadCorpus()
         model.getData(corpus_df)
         model.train(corpus_df)
         out = model.output()
-        print(f"NODE {self.settings['node']} END")
         return out
 
 
